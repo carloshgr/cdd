5f6c65b8f0d38e4ea405c9217e3b7f819ef7aed3
carloshgr <carlos.ribeiro@ccc.ufcg.edu.br>
2022-11-15 18:20:49 -0300

    adicionando nome e genero a deputados.csv

--- lab3/spiders/deputados_spider.py
+++ lab3/spiders/deputados_spider.py
@@ -4,15 +4,40 @@ class DeputadosSpider(scrapy.Spider):
     name = 'deputados'
 
     def start_requests(self):
-        urls = ['https://www.camara.leg.br/deputados/quem-sao/resultado?search=&partido=&uf=&legislatura=56&sexo=M&pagina='+str(i) for i in range(1, 22)]
+        deputados_pages = ['https://www.camara.leg.br/deputados/quem-sao/resultado?search=&partido=&uf=&legislatura=56&sexo=M&pagina='+str(i) for i in range(1, 22)]
+        deputadas_pages = ['https://www.camara.leg.br/deputados/quem-sao/resultado?search=&partido=&uf=&legislatura=56&sexo=F&pagina='+str(i) for i in range(1, 5)]
         
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_pages)
+        for page in deputados_pages:
+            yield scrapy.Request(url=page, callback=self.request_deputados)
 
-    def parse_pages(self, response):
+        for page in deputadas_pages:
+            yield scrapy.Request(url=page, callback=self.request_deputadas)
+
+
+    def request_deputados(self, response):
         for tag in response.xpath('//h3[@class="lista-resultados__cabecalho"]/a/@href'):
-            yield scrapy.Request(url=tag.get(), callback=self.parse_deputados)
+            yield scrapy.Request(url=tag.get(), callback=self.parse_deputado)
 
-    def parse_deputados(self, response):
-        tag = response.xpath('//h2[@id="nomedeputado"]//text()')
-        yield {'nome': tag.get()}
+
+    def request_deputadas(self, response):
+        for tag in response.xpath('//h3[@class="lista-resultados__cabecalho"]/a/@href'):
+            yield scrapy.Request(url=tag.get(), callback=self.parse_deputada)
+
+
+    def get_nome(self, response):
+        tag_nome = response.xpath('//h2[@id="nomedeputado"]//text()')
+        return tag_nome.get()
+
+
+    def parse_deputado(self, response):
+        yield {
+            'nome': self.get_nome(response),
+            'genero': 'M'
+        }
+
+
+    def parse_deputada(self, response):
+        yield {
+            'nome': self.get_nome(response),
+            'genero': 'F'
+        }
